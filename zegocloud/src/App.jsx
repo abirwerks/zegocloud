fa3e6e4d266a8b26c52987421cea31319748b84a
abirwerks <abirwerks@gmail.com>
2024-09-25 10:08:47 +0600

    unnecessary imports removed

--- zegocloud/src/App.jsx
+++ zegocloud/src/App.jsx
@@ -1,11 +1,8 @@
-import { useState } from "react";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/Home";
 import RoomPage from "./pages/Room";
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <Routes>
       <Route path="/" element={<HomePage />}></Route>
